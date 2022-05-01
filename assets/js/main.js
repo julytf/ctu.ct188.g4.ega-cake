07ac838e2f47b2ef2197875f76452681d19e135f
julyyv <chanvucktv@gmail.com>
2022-05-01 19:40:53 +0700

    fix path

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,32 @@ Object.values($$('.close')).forEach((entity, index)=>{
         e.target.parentElement.remove()
     }
 })
+
+var sliderRadios = $$('.slider__radio')
+
+var prevSlide = $('.slider__prev')
+var nextSlide = $('.slider__next')
+
+
+autoSlider(sliderRadios, 5*1000)
+
+prevSlide.onclick = () => prevRadio(sliderRadios)
+nextSlide.onclick = () => nextRadio(sliderRadios)
+
+function prevRadio(radios) {
+    let current = Object.values(radios).findIndex(radio=>radio.checked)
+    let next = (current + radios.length - 1) % radios.length
+    radios[next].checked = true
+}
+
+function nextRadio(radios) {
+    let current = Object.values(radios).findIndex(radio=>radio.checked)
+    let next = (current + 1) % radios.length
+    radios[next].checked = true
+}
+
+function autoSlider(radios, delay) {
+    setInterval(()=>{
+        nextRadio(radios)
+    }, delay)
+}
